aaf4b7f80cf8d9259c8de5c2aeded2dba3953691
stodipo <odipo87@gmail.com>
2018-07-25 17:07:22 +0300

    Updated the dashboard contents

--- admin/class_sidebar.php
+++ admin/class_sidebar.php
@@ -1,10 +1,10 @@
      <div class="span3" id="sidebar">
-                    <ul class="nav nav-list bs-docs-sidenav nav-collapse collapse">
+     <ul class="nav nav-list bs-docs-sidenav nav-collapse collapse">
                         <li> <a href="dashboard.php"><i class="icon-chevron-right"></i><i class="icon-home"></i>&nbsp;Dashboard</a> </li>
                         <li>
                             <a href="department.php"><i class="icon-chevron-right"></i><i class="icon-building"></i> County Heads</a>
                         </li>
-                        <li class="active">
+                        <li class"active">
                             <a href="class.php"><i class="icon-chevron-right"></i><i class="icon-group"></i> Departments</a>
                         </li>
                         <li>
@@ -38,7 +38,6 @@
                             <a href="calendar_of_events.php"><i class="icon-chevron-right"></i><i class="icon-calendar"></i>Calendar of Events</a>
                         </li>
                     </ul>
-					
 					
 				
                 </div>
--- admin/teacher_sidebar.php
+++ admin/teacher_sidebar.php
@@ -1,29 +1,28 @@
-     <div class="span3" id="sidebar">
-                    <ul class="nav nav-list bs-docs-sidenav nav-collapse collapse">
+<ul class="nav nav-list bs-docs-sidenav nav-collapse collapse">
                         <li> <a href="dashboard.php"><i class="icon-chevron-right"></i><i class="icon-home"></i>&nbsp;Dashboard</a> </li>
-						<li>
+                        <li>
+                            <a href="department.php"><i class="icon-chevron-right"></i><i class="icon-building"></i> County Heads</a>
+                        </li>
+                        <li>
+                            <a href="class.php"><i class="icon-chevron-right"></i><i class="icon-group"></i> Departments</a>
+                        </li>
+                        <li class"active">
+                            <a href="teachers.php"><i class="icon-chevron-right"></i><i class="icon-group"></i> Training Administrator</a>
+                        </li>
+                        <li>
                             <a href="subjects.php"><i class="icon-chevron-right"></i><i class="icon-list-alt"></i> Trainings</a>
                         </li>
 						<li>
-                            <a href="class.php"><i class="icon-chevron-right"></i><i class="icon-group"></i> Departments</a>
-                        </li>
-						<li>
-                            <a href="admin_user.php"><i class="icon-chevron-right"></i><i class="icon-user"></i> Admin Users</a>
-                        </li>
-						<li>
-                            <a href="department.php"><i class="icon-chevron-right"></i><i class="icon-building"></i> Departmental heads</a>
-                        </li>
-						<li>
                             <a href="students.php"><i class="icon-chevron-right"></i><i class="icon-group"></i> Employees</a>
-                        </li>
-						<li  class="active">
-                            <a href="teachers.php"><i class="icon-chevron-right"></i><i class="icon-group"></i> Head of Training</a>
-                        </li>
-						<li>
+                        </li>	
+							<li>
                             <a href="downloadable.php"><i class="icon-chevron-right"></i><i class="icon-download"></i> Downloadable Materials</a>
                         </li>
 						<li>
                             <a href="content.php"><i class="icon-chevron-right"></i><i class="icon-file"></i> Content</a>
+                        </li>
+                        <li>
+                            <a href="admin_user.php"><i class="icon-chevron-right"></i><i class="icon-user"></i> Admin Users</a>
                         </li>
 						<li>
                             <a href="user_log.php"><i class="icon-chevron-right"></i><i class="icon-file"></i> User Log</a>
@@ -31,14 +30,12 @@
 						<li>
                             <a href="activity_log.php"><i class="icon-chevron-right"></i><i class="icon-file"></i> Activity Log</a>
                         </li>
-						<li>
+                        <li>
                             <a href="school_year.php"><i class="icon-chevron-right"></i><i class="icon-calendar"></i> School Year</a>
                         </li>
 						<li class="">
                             <a href="calendar_of_events.php"><i class="icon-chevron-right"></i><i class="icon-calendar"></i>Calendar of Events</a>
                         </li>
                     </ul>
-					
-					
 				
                 </div>
