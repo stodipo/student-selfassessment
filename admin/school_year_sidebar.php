4ce6dd4922868e09a17d689d535a165fa77ad227
stodipo <odipo87@gmail.com>
2018-07-27 16:26:09 +0300

    Updated the year contents

--- admin/school_year_sidebar.php
+++ admin/school_year_sidebar.php
@@ -32,7 +32,7 @@
                             <a href="activity_log.php"><i class="icon-chevron-right"></i><i class="icon-file"></i> Activity Log</a>
                         </li>
                         <li class="active">
-                            <a href="school_year.php"><i class="icon-chevron-right"></i><i class="icon-calendar"></i> School Year</a>
+                            <a href="school_year.php"><i class="icon-chevron-right"></i><i class="icon-calendar"></i> Year</a>
                         </li>
 						<li class="">
                             <a href="calendar_of_events.php"><i class="icon-chevron-right"></i><i class="icon-calendar"></i>Calendar of Events</a>
