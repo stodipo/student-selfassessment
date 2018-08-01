73f9d82d38528c1b263252a57bc47591d290a781
stodipo <odipo87@gmail.com>
2018-08-01 21:03:53 +0300

    Modified the employees panel

--- add_announcement.php
+++ add_announcement.php
@@ -15,7 +15,7 @@
 								$school_year = $school_year_query_row['school_year'];
 								?>
 								<li><a href="#"><b>My Class</b></a><span class="divider">/</span></li>
-								<li><a href="#">School Year: <?php echo $school_year_query_row['school_year']; ?></a></li>
+								<li><a href="#">Year: <?php echo $school_year_query_row['school_year']; ?></a></li>
 						</ul>
 						 <!-- end breadcrumb -->
 					 
