7c35c353a416926e8e80d795adff35e43b9411b6
stodipo <odipo87@gmail.com>
2018-08-28 23:13:35 +0300

    Updated the images

--- link.php
+++ link.php
@@ -1,5 +1,3 @@
-
-
 <div class="navbar">
            <div class="navbar-inner">
                <div class="container-fluid">
@@ -11,17 +9,20 @@
 							<div class="nav-collapse collapse">
 							<ul class="nav" id="footer_nav">
 			<li class="divider-vertical"></li>
-		<li class="active"><a href="index.php"><i class="icon-home"></i><font color="purple"><B>&nbsp;HOME</B></font></a></li>
+		<li class="active"><a href="index.php"><i class="icon-home"></i><B>&nbsp;HOME</B></font></a></li>
 			<li class="divider-vertical"></li>
-		<li class="active"><a href="about.php"><i class="icon-info-sign"></i><font color="purple"><b>&nbsp;ABOUT</b></font></a></li>
+			<li class="active"><a href="about.php"><i class="icon-info-sign"></i><b>&nbsp;ABOUT</b></font></a></li>
 			<li class="divider-vertical"></li>
-		<li class="active"><a href="calendar_of_events.php"><i class="icon-calendar"></i><font color="purple"><b>&nbsp;CALENDER OF EVENTS</b></font></a></li>
+		<li class="active"><a href="calendar_of_events.php"><i class="icon-calendar"></i><b>&nbsp;CALENDER OF EVENTS</b></font></a></li>
 			<li class="divider-vertical"></li>
-		<li class="active"><a href="developers.php"><i class="icon-user"></i><font color="purple"><b>&nbsp;DEVELOPERS</b></font></a></li>
+		<li class="active"><a href="developers.php"><i class="icon-user"></i><b>&nbsp;DEVELOPERS</b></font></a></li>
 			<li class="divider-vertical"></li>
-		<li class="active"><a href="#campuses" data-toggle="modal"><i class="icon-building"></i><font color="purple"><b>&nbsp;CAMPUSES</b</font></a></li>
+			<li class="active"><a href="contactus.php"><i class="icon-user"></i><b>&nbsp;CONTACT US</b></font></a></li>
+			<li class="divider-vertical"></li>
+		<li class="active"><a href="#campuses" data-toggle="modal"><i class="icon-building"></i><b>&nbsp;CAMPUSES</b</font></a></li>
 				<?php include('campuses.php'); ?>
 			<li class="divider-vertical"></li>
+
 	
 	</ul>
 							</div>
