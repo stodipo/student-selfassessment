ab479e3ff0e642a661edc9627080b66107ce451a
stodipo <odipo87@gmail.com>
2018-08-29 16:53:55 +0300

    Updated the Admin Dashboard files

--- navbar_about.php
+++ navbar_about.php
@@ -5,7 +5,7 @@
 						<span class="icon-bar"></span>
 						<span class="icon-bar"></span>
 					</a>
-                   <a class="brand" href="#">Welcome to the Academic Resource System</a>
+                   <a class="brand" href="#">Welcome to the Student Self-Assessment System</a>
 							<div class="nav-collapse collapse">
 								<ul class="nav pull-right">
 	
